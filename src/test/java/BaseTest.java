57413a7355935a0d53124539ea608f7bf6f58433
czar-testpro <czar@testpro.io>
2024-08-06 21:37:02 +0800

    Added L19 examples

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,16 +4,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.time.Duration;
+import java.util.UUID;
 
 public class BaseTest {
 
-    public WebDriver driver;
-    public String url = "https://qa.koel.app/";
+    public WebDriver driver = null;
+    public String url = null;
+
+    @DataProvider(name="IncorrectLoginData")
+    public static Object[][] getDataFromDataProviders() {
+        return new Object[][] {
+                {"invalid@testpro.io", "invalidPassword"},
+                {"demo@testpro.io", ""},
+                {"", ""}
+        };
+    }
+
+    //without static keyword
+//    @DataProvider(name="IncorrectLoginData")
+//    public Object[][] getDataFromDataProviders() {
+//        return new Object[][] {
+//                {"invalid@testpro.io", "invalidPassword"},
+//                {"demo@testpro.io", ""},
+//                {"", ""}
+//        };
+//    }
 
     @BeforeSuite
     static void setupClass() {
@@ -21,38 +39,51 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void launchBrowser(){
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL) {
+        //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
+        url = BaseURL;
         navigateToPage();
     }
 
     @AfterMethod
-    public void closeBrowser(){
+    public void closeBrowser() {
         driver.quit();
     }
-
-    void clickLoginBtn() {
-        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-        submit.click();
+    public  void navigateToPage() {
+        driver.get(url);
     }
-
-    public void providePassword(String password) {
-        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        passwordField.clear();
-        passwordField.sendKeys(password);
-    }
-
     public void provideEmail(String email) {
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
         emailField.sendKeys(email);
     }
-
-    public void navigateToPage() {
-        driver.get(url);
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
     }
+    public void clickLoginBtn() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+    public void provideProfileName(String randomName) {
+        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
+        profileName.clear();
+        profileName.sendKeys(randomName);
+    }
+    public void provideCurrentPassword(String password) {
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
+    }
+    public String generateRandomName() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
 }
